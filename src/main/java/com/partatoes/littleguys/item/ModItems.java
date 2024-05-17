7f8b422e5b99134f9e377e1c9e80e767ea065003
jperoutek <jperoutek@gmail.com>
2024-05-16 22:32:47 -0500

    Convert item to SpawnEggItem to allow spawning and Spawner usage

--- src/main/java/com/partatoes/littleguys/item/ModItems.java
+++ src/main/java/com/partatoes/littleguys/item/ModItems.java
@@ -1,18 +1,19 @@
 package com.partatoes.littleguys.item;
 
 import com.partatoes.littleguys.LittleGuys;
-import com.partatoes.littleguys.item.custom.LittleGuyItem;
+import com.partatoes.littleguys.entity.ModEntities;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.SpawnEggItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item LITTLEGUY_ITEM = registerItem("littleguy_item", new LittleGuyItem(new Item.Settings()));
+    public static final Item LITTLEGUY_ITEM = registerItem("littleguy_item", new SpawnEggItem(ModEntities.LITTLEGUY_ENTITY, 13158600, 13158600, new Item.Settings()));
     public static final Item SOULSAND_PILE_ITEM = registerItem("soulsand_pile", new Item(new Item.Settings()));
 
     public static Item registerItem(String name, Item item) {
--- src/main/java/com/partatoes/littleguys/item/custom/LittleGuyItem.java
+++
@@ -1,9 +0,0 @@
-package com.partatoes.littleguys.item.custom;
-
-import net.minecraft.item.Item;
-
-public class LittleGuyItem extends Item {
-    public LittleGuyItem(Settings settings) {
-        super(settings);
-    }
-}
