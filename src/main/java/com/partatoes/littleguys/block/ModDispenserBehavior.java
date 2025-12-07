9caac8bccaa405282172f97a76a8e448ebe7f3c0
jperoutek <jperoutek@gmail.com>
2025-12-07 12:25:33 -0600

    Allow little guys to be cleaned by right-clicking with a sponge (wet or dry), or via dispensing a sponge.
    Allow dyeing via dispenser.

--- src/main/java/com/partatoes/littleguys/block/ModDispenserBehavior.java
+++ src/main/java/com/partatoes/littleguys/block/ModDispenserBehavior.java
@@ -7,13 +7,20 @@ import com.partatoes.littleguys.item.custom.LittleGuySpawnEggItem;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.dispenser.ItemDispenserBehavior;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.SpawnEggItem;
+import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.item.*;
+import net.minecraft.util.DyeColor;
 import net.minecraft.util.math.BlockPointer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.event.GameEvent;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class ModDispenserBehavior {
     public static ItemDispenserBehavior LITTLEGUY_DISPENSER_BEHAVIOR = new ItemDispenserBehavior(){
@@ -43,14 +50,64 @@ public class ModDispenserBehavior {
             return stack;
         }
     };
+
+    public static ItemDispenserBehavior DYE_LITTLEGUY_DISPENSER_BEHAVIOR = new ItemDispenserBehavior() {
+        @Override
+        protected ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack) {
+            BlockPos blockPos = pointer.pos().offset(pointer.state().get(DispenserBlock.FACING));
+            List<LittleGuyEntity> list = pointer.world().getEntitiesByClass(LittleGuyEntity.class, new Box(blockPos), entity -> true);
+            if (list.isEmpty()) {
+                return super.dispenseSilently(pointer, stack);
+            } else {
+                LittleGuyEntity littleGuyEntity = list.getFirst();
+
+                ItemStack itemStack = stack.split(1);
+                DyeColor color = ((DyeItem) itemStack.getItem()).getColor();
+
+                littleGuyEntity.setColor(color);
+                littleGuyEntity.setIsNeutral(false);
+
+                return stack;
+            }
+        }
+    };
+
+    public static ItemDispenserBehavior CLEAN_LITTLEGUY_DISPENSER_BEHAVIOR = new ItemDispenserBehavior() {
+        @Override
+        protected ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack) {
+            BlockPos blockPos = pointer.pos().offset(pointer.state().get(DispenserBlock.FACING));
+            List<LittleGuyEntity> list = pointer.world().getEntitiesByClass(LittleGuyEntity.class, new Box(blockPos), entity -> !entity.isNeutral());
+            if (list.isEmpty()) {
+                return super.dispenseSilently(pointer, stack);
+            } else {
+                for (LittleGuyEntity littleGuyEntity : list) {
+                    littleGuyEntity.setColor(DyeColor.WHITE);
+                    littleGuyEntity.setIsNeutral(true);
+                }
+                return stack;
+            }
+        }
+    };
+
+
     public static void registerDispenserBehaviors() {
         LittleGuys.LOGGER.info("Registering dispenser behaviors for " + LittleGuys.MOD_ID);
 
+        // Register code for dispensing the Little Guys
         DispenserBlock.registerBehavior(ModItems.LITTLEGUY_ITEM, LITTLEGUY_DISPENSER_BEHAVIOR);
         DispenserBlock.registerBehavior(ModItems.LITTLEHORSE_ITEM, LITTLEGUY_DISPENSER_BEHAVIOR);
-
         for (Item spawnEggItem : ModItems.LITTLEGUY_COLORS.values()) {
             DispenserBlock.registerBehavior(spawnEggItem, LITTLEGUY_DISPENSER_BEHAVIOR);
         }
+
+        // Register code for dyeing little guys using dispensers
+        for (DyeColor color : DyeColor.values()) {
+            DyeItem dye = DyeItem.byColor(color);
+            DispenserBlock.registerBehavior(dye, DYE_LITTLEGUY_DISPENSER_BEHAVIOR);
+        }
+
+        // Register code for cleaning Little Guys using sponges and dispensers
+        DispenserBlock.registerBehavior(Items.SPONGE, CLEAN_LITTLEGUY_DISPENSER_BEHAVIOR);
+        DispenserBlock.registerBehavior(Items.WET_SPONGE, CLEAN_LITTLEGUY_DISPENSER_BEHAVIOR);
     }
 }
--- src/main/java/com/partatoes/littleguys/entity/custom/LittleGuyEntity.java
+++ src/main/java/com/partatoes/littleguys/entity/custom/LittleGuyEntity.java
@@ -2,6 +2,8 @@ package com.partatoes.littleguys.entity.custom;
 
 import com.partatoes.littleguys.entity.goal.BoardLittleHorseGoal;
 import com.partatoes.littleguys.item.ModItems;
+import net.minecraft.block.SpongeBlock;
+import net.minecraft.block.WetSpongeBlock;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
@@ -12,14 +14,19 @@ import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.mob.PathAwareEntity;
-import net.minecraft.item.Item;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.*;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.storage.ReadView;
 import net.minecraft.storage.WriteView;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.DyeColor;
+import net.minecraft.util.Hand;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
 import net.minecraft.world.World;
+import net.minecraft.world.event.GameEvent;
 import org.jetbrains.annotations.Nullable;
 
 import static com.partatoes.littleguys.entity.ModEntities.COLOR_LITTLEGUY_BIMAP;
@@ -39,7 +46,8 @@ public class LittleGuyEntity extends PathAwareEntity {
         this.goalSelector.add(0, new SwimGoal(this));
         this.goalSelector.add(1, new MeleeAttackGoal(this, 1, false));
         this.goalSelector.add(2, new BoardLittleHorseGoal(this));
-        this.goalSelector.add(3, new WanderAroundGoal(this, .3));
+        this.goalSelector.add(3, new TemptGoal(this, 1.2, stack -> stack.isOf(Items.CLAY_BALL), false, 3));
+        this.goalSelector.add(4, new WanderAroundGoal(this, .3));
 
         this.targetSelector.add(0, new RevengeGoal(this));
         this.targetSelector.add(1, new ActiveTargetGoal<>(this, LittleGuyEntity.class, 1, true, true, this::canLittleGuyAttackTarget));
@@ -49,30 +57,41 @@ public class LittleGuyEntity extends PathAwareEntity {
         return MobEntity.createMobAttributes()
                 .add(EntityAttributes.MAX_HEALTH, 6)
                 .add(EntityAttributes.MOVEMENT_SPEED, .35)
-                .add(EntityAttributes.ATTACK_DAMAGE, .5);
+                .add(EntityAttributes.ATTACK_DAMAGE, .5)
+                .add(EntityAttributes.TEMPT_RANGE, 5);
     }
-
-    // Required for previous versions, likely to be removed in the future
-//    private static float[] getDyedColor(DyeColor color) {
-//        if (color == DyeColor.WHITE) {
-//            return new float[]{0.9019608F, 0.9019608F, 0.9019608F};
-//        } else {
-//            int colorInt = color.getEntityColor();
-//            float colorScaler = 0.75f;
-//
-//            return new float[]{
-//                    ColorHelper.getRed(colorInt) * colorScaler,
-//                    ColorHelper.getGreen(colorInt) * colorScaler,
-//                    ColorHelper.getBlue(colorInt) * colorScaler
-//            };
-//        }
-//    }
 
     @Override
     protected void initDataTracker(DataTracker.Builder builder) {
         super.initDataTracker(builder);
         builder.add(COLOR, (byte) COLOR_LITTLEGUY_BIMAP.inverse().getOrDefault(this.getType(), DyeColor.WHITE).getIndex());
         builder.add(IS_NEUTRAL, this.getType().equals(LITTLEGUY_ENTITY));
+    }
+
+    @Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        if (itemStack.getItem() instanceof DyeItem dyeItem && dyeItem.getColor() != this.getColor()) {
+            if (this.getEntityWorld() instanceof ServerWorld) {
+                itemStack.decrement(1);
+                this.setColor(dyeItem.getColor());
+                this.setIsNeutral(false);
+                return ActionResult.SUCCESS_SERVER;
+            } else {
+                return ActionResult.CONSUME;
+            }
+        } else if (itemStack.getItem() instanceof BlockItem blockItem)  {
+            if (blockItem.getBlock() instanceof SpongeBlock || blockItem.getBlock() instanceof WetSpongeBlock) {
+                if (this.getEntityWorld() instanceof ServerWorld) {
+                    this.setColor(DyeColor.WHITE);
+                    this.setIsNeutral(true);
+                    return ActionResult.SUCCESS_SERVER;
+                } else {
+                    return ActionResult.CONSUME;
+                }
+            }
+        }
+        return super.interactMob(player, hand);
     }
 
     public DyeColor getColor() {
